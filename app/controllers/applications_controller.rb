117e2e08b878e219f8b75757b0a816e48f455a44
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-15 14:02:26 -0400

    US_5_wip

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,12 +3,13 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+    @cart = Cart.new(session[:cart])
+    @pets_search = []
     
-    if params[:pet_name_search] != nil
+    if params[:pet_name_search].present?
       @pets_search = Pet.case_insenstive_search(params[:pet_name_search])
-    else
-      @pets_search = []
     end
+
   end
 
   def new
