2d7579fc33fb99a78cfc583a1f785727b5f2e34e
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-16 18:25:09 -0400

    Complete db_logic admin/shelter/index US

---
+++ app/controllers/admin/shelters_controller.rb
@@ -0,0 +1,7 @@
+class Admin::SheltersController < ApplicationController
+
+  def index
+    @shelters = Shelter.order_by_reverse_alphabetical
+  end
+
+end
--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'the admin shelter index' do
   it 'lists all the shelter names in reverse alphabetical order' do
-    @shelter_1 = Shelter.create(name: 'C - Test', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: 'D - Test', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: 'A - Test', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_4 = Shelter.create(name: 'B - Test', city: 'Denver, CO', foster_program: true, rank: 10)
+    @shelter_c = Shelter.create(name: 'C - Test', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter_d = Shelter.create(name: 'D - Test', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    @shelter_a = Shelter.create(name: 'A - Test', city: 'Denver, CO', foster_program: true, rank: 10)
+    @shelter_b = Shelter.create(name: 'B - Test', city: 'Denver, CO', foster_program: true, rank: 10)
 
     visit '/admin/shelters'
 
-    expect(@shelter_3.name).to appear_before(@shelter_4.name)
+    expect(@shelter_d.name).to appear_before(@shelter_c.name)
 
-    expect(@shelter_4.name).to appear_before(@shelter_1.name)
+    expect(@shelter_c.name).to appear_before(@shelter_b.name)
 
-    expect(@shelter_1.name).to appear_before(@shelter_2.name)
+    expect(@shelter_b.name).to appear_before(@shelter_a.name)
 
-    expect(page).to have_content(@shelter_2.name)
+    expect(page).to have_content(@shelter_a.name)
   end
 end
