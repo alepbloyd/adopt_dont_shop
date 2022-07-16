3cc50ec464d505a401b2212d43c5e02aa8010d0b
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-15 20:31:06 -0400

    Most of US_5 functional

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,19 @@ class Cart
   attr_reader :contents
 
   def initialize(initial_contents)
-    @content = initial_contents
+    @contents = initial_contents || Hash.new(0)
+  end
+
+  def count
+    @contents.length
+  end
+
+  def add_pet(id)
+    @contents[id.to_s] = count + 1
+  end
+
+  def show_pet
+    "hello"
   end
 
 end
