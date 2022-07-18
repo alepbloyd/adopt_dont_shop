29d7f567041bb332fd1e8f6a92012dcfed2c73ab
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-17 20:49:12 -0400

    Add pet_app_status to application_pets join table

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -23,6 +23,35 @@ RSpec.describe 'admin application show page' do
     expect(page).to have_content(pet_2.name)
     expect(page).to have_content(pet_3.name)
     expect(page).to_not have_content(pet_4.name)
+  end
+
+  it 'has a button to approve application for specific pet, which takes user back to admin/application/show page, where the button for that pet is gone and there is an indication that the pet is approved' do
+
+    shelter_1 = FactoryBot.create(:shelter)
+
+    pet_1 = FactoryBot.create(:pet, shelter: shelter_1)
+    pet_2 = FactoryBot.create(:pet, shelter: shelter_1)
+    pet_3 = FactoryBot.create(:pet, shelter: shelter_1)
+    pet_4 = FactoryBot.create(:pet, shelter: shelter_1)
+
+    application_1 = FactoryBot.create(:application)
+
+    application_pet_1 = FactoryBot.create(:application_pet, application: application_1, pet: pet_1)
+    application_pet_1 = FactoryBot.create(:application_pet, application: application_1, pet: pet_2)
+    application_pet_1 = FactoryBot.create(:application_pet, application: application_1, pet: pet_3)
+
+    visit "/admin/applications/#{application_1.id}"
+
+    within '#pet-1' do
+      click_link("Approve This Pet")
+    end
+
+    expect(current_path).to eq("/admin/applications/#{application_1.id}")
+
+    within "#pet-1" do
+      expect(page).to_not have_content("Approve This Pet")
+      expect(page).to have_content("Approved!")
+    end
 
   end
 
