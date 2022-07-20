121ca3c59c9e9035ded9eab9daf2ade452621a16
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-20 11:13:18 -0400

    More testing, app_pet returns pet name

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe ApplicationPet, type: :model do
     it {should belong_to :application}
     it {should belong_to :pet}
   end
+
+  it 'should return name of pet associated with application_pet' do
+    shelter_1 = FactoryBot.create(:shelter)
+    pet_1 = FactoryBot.create(:pet, shelter: shelter_1)
+    application_1 = FactoryBot.create(:application)
+
+    application_pet = FactoryBot.create(:application_pet, pet: pet_1, application: application_1)
+
+    expect(application_pet.pet_name).to eq(pet_1.name)
+  end
 end
