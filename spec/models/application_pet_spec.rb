d3eebb37ba6e4ffa969b59a7a94949de1a37698c
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-20 11:22:16 -0400

    Test: application_pet order testing

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe ApplicationPet, type: :model do
 
     expect(application_pet.pet_name).to eq(pet_1.name)
   end
+
+  it 'returns application_pets in order of recently created first' do
+    shelter_1 = FactoryBot.create(:shelter)
+
+    pet_1 = FactoryBot.create(:pet, shelter: shelter_1)
+    pet_2 = FactoryBot.create(:pet, shelter: shelter_1)
+    pet_3 = FactoryBot.create(:pet, shelter: shelter_1)
+
+    application_1 = FactoryBot.create(:application)
+    application_2 = FactoryBot.create(:application)
+    application_3 = FactoryBot.create(:application)
+
+    application_pet_1 = FactoryBot.create(:application_pet, pet: pet_1, application: application_1,created_at: "2020-07-13 17:56:48.851122")
+    application_pet_2 = FactoryBot.create(:application_pet, pet: pet_1, application: application_2,created_at: "2022-07-13 17:56:48.851122")
+    application_pet_3 = FactoryBot.create(:application_pet, pet: pet_1, application: application_3, created_at: "2021-07-13 17:56:48.851122")
+
+    expect(ApplicationPet.order_by_recently_created).to eq([application_pet_2,application_pet_3,application_pet_1])
+  end
 end
